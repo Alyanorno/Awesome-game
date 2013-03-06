48894b413faa0398cadd343a1e79e1bae3b3a2ee
Rickard Fridvall <partim@live.com>
2013-03-06 18:54:26 +0100

    Modified .bmp files

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-cl /EHsc /Zi /MDd Source/main.cpp Source/graphic.cpp Source/input.cpp Source/logic.cpp Source/globals.cpp opengl32.lib GL/lib/GLFWDLL.lib GL/lib/glew32.lib GL/lib/glew32s.lib
+cls
+cl /EHsc /Zi /MDd Source/main.cpp Source/graphic.cpp Source/input.cpp Source/logic.cpp Source/globals.cpp Source/Type/farm.cpp Source/Type/city.cpp Source/Type/structure.cpp Source/Type/army.cpp opengl32.lib GL/lib/GLFWDLL.lib GL/lib/glew32.lib GL/lib/glew32s.lib
